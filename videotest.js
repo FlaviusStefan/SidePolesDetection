c463cbb612ccef20c8b85253e853b85d540e5cf3
Vlad <r.vlad7@yahoo.com>
2023-11-13 15:33:27 +0200

    removed gray filter from the second video

--- videotest.js
+++ videotest.js
@@ -10,20 +10,15 @@ startSource_Video();
 
 cv['onRuntimeInitialized'] = () => {
     let srcImage = new cv.Mat(Source_video.height, Source_video.width, cv.CV_8UC4);
-    let dstImage = new cv.Mat(Source_video.height, Source_video.width, cv.CV_8UC1);
     let cap = new cv.VideoCapture(Source_video);
 
     const FPS = 30;
     function processSource_Video() {
         let begin_delay = Date.now();
         cap.read(srcImage);
-        cv.cvtColor(srcImage, dstImage, cv.COLOR_RGBA2GRAY);
-        cv.imshow(canvas, dstImage);
+        cv.imshow(canvas, srcImage);
         let delay = 1000 / FPS - (Date.now() - begin_delay);
         setTimeout(processSource_Video, delay);
-    };
+    };    
     setTimeout(processSource_Video, 0);
 };
-
-
-//test ca merge colaborarea anului
